732d3d1bf753fd200ab741772fb73f57da7fbe7a
Ryn <bridgelab@musc.edu>
2023-09-18 16:43:49 -0400

    update pyd.sh

--- pydesigner_v1.0/pyd.sh
+++ pydesigner_v1.0/pyd.sh
@@ -11,17 +11,21 @@ BGreen='\033[1;32m' # Bold Green
 NC='\033[0m' # No Color
 
 read -p $'\033[1;36mEnter subject IDs to process separated only by a space (eg. M101 M102 M103):\e[0m ' SUBJ_IDs
-read -p $'\033[1;36mEnter path to study folder:\e[0m ' base
-read -p $'\033[1;36mEnter DKI file name to process. Eg. DKI_30.nii or DKI_BIPOLAR_2.5_64dir_50slices.nii.gz:\e[0m ' DKI
-read -p $'\033[1;36mEnter TOPUP file name to process. Eg. DKI_30_TOPUP.nii or DKI_BIPOLAR_2.5_64dir_50slices_TOPUP.nii.gz (leave blank if subject does not have TOPUP data):\e[0m ' TOPUP
+read -p $'\033[1;36mEnter path to study folder (the main folder housing all of your files for this process):\e[0m ' base
+read -p $'\033[1;36mEnter DKI file name to process INCLUDING file extension. Eg. DKI_30.nii or DKI_BIPOLAR_2.5_64dir_50slices.nii.gz or etc:\e[0m ' DKI
+read -p $'\033[1;36mEnter TOPUP file name to process INCLUDING file extension. Eg. DKI_30_TOPUP.nii or DKI_BIPOLAR_2.5_64dir_50slices_TOPUP.nii.gz or etc (leave blank if subject does not have TOPUP data):\e[0m ' TOPUP
+
+mkdir -p $base/03_Analysis
 
 for i in $SUBJ_IDs ; do 
-  echo -e "${BYellow}Beginning PyDesigner for ${i}${NC}"
-  mkdir $base/03_Analysis/${i}
-  cp $base/02_Data/${i}/$DKI $base/03_Analysis/${i}/DKI.nii
-  DKIn = $base/03_Analysis/${i}/DKI.nii
-  [ -f "$base/02_Data/${i}/$TOPUP" ] && cp "$base/02_Data/${i}/$TOPUP" "$base/03_Analysis/${i}/DKI_TOPUP.nii"
-  TOPUPn = $base/03_Analysis/${i}/DKI_TOPUP.nii
+  echo -e "${BYellow}Beginning file prep ${i}${NC}"
+  mkdir -p $base/03_Analysis/${i}
+  cp $base/02_Data/${i}/**/$DKI $base/03_Analysis/${i}/DKI.nii
+  DKIn=$base/03_Analysis/${i}/DKI.nii
+  [ -f $base/02_Data/${i}/**/$TOPUP ] && cp $base/02_Data/${i}/**/$TOPUP $base/03_Analysis/${i}/DKI_TOPUP.nii
+  TOPUPn=$base/03_Analysis/${i}/DKI_TOPUP.nii
   inputs="$DKIn $TOPUPn"
+  echo -e "${BYellow}Beginning PyDesigner for ${i}${NC}"
+  conda activate dmri
   pydesigner -o $base/03_Analysis/${i} -s $inputs
 done
