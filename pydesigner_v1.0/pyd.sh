edf2a95c8a381a18cc78bd507a1a95ac9b070fd8
Ryn <bridgelab@musc.edu>
2023-09-18 16:16:10 -0400

    update pyd.sh file iding

--- pydesigner_v1.0/pyd.sh
+++ pydesigner_v1.0/pyd.sh
@@ -1,15 +1,9 @@
 ################################
 #
-# dicom convert and sort
+# PyDesigner processing
 # Ryn Thorn - 3.30.23
 #
 ################################
-
-
-export base=/Volumes/vdrive/helpern_users/benitez_a/PUMA/PUMA_Imaging_reorg
-export analysis=/Volumes/vdrive/helpern_users/benitez_a/PUMA/PUMA_Analysis_reorg/pydesigner_v1.0
-export nii=$base/PUMA_Imaging_reorg/03_nii
-export pyd=$base/PUMA_Analysis_reorg/pydesigner_v1.0
 
 BCyan='\033[1;36m' # Bold Cyan
 BYellow='\033[1;33m' # Bold Yellow
@@ -18,20 +12,16 @@ NC='\033[0m' # No Color
 
 read -p $'\033[1;36mEnter subject IDs to process separated only by a space (eg. M101 M102 M103):\e[0m ' SUBJ_IDs
 read -p $'\033[1;36mEnter path to study folder:\e[0m ' base
-read -p $'\033[1;36mEnter path to raw nifti files:\e[0m ' niis
+read -p $'\033[1;36mEnter DKI file name to process. Eg. DKI_30.nii or DKI_BIPOLAR_2.5_64dir_50slices.nii.gz:\e[0m ' DKI
+read -p $'\033[1;36mEnter TOPUP file name to process. Eg. DKI_30_TOPUP.nii or DKI_BIPOLAR_2.5_64dir_50slices_TOPUP.nii.gz (leave blank if subject does not have TOPUP data):\e[0m ' TOPUP
 
 for i in $SUBJ_IDs ; do 
-  echo -e "${BYellow}Setting up files${NC}"
-  mkdir -p $analysis/02_Data/${i}
-  for f in nii bvec bval json ; do
-    cp $base/03_nii/${i}/DKI_BIPOLAR_2.5mm_64dir_50slices.${f} $analysis/02_Data/${i}/DKI.${f}
-  done
-  for f in nii json ; do
-    cp $base/03_nii/${i}/DKI_BIPOLAR_2.5mm_64dir_50slices_TOP_UP_PA.${f} $analysis/02_Data/${i}/DKI_TOPUP.${f}
-  done
-done
-  
-for i in $SUBJ_IDs ; do 
-  mkdir $analysis/03_Analysis/${i}
-  pydesigner -o $analysis/03_Analysis/${i} -s $analysis/02_Data/${i}/DKI.nii $analysis/02_Data/${i}/DKI_TOPUP.nii
+  echo -e "${BYellow}Beginning PyDesigner for ${i}${NC}"
+  mkdir $base/03_Analysis/${i}
+  cp $base/02_Data/${i}/$DKI $base/03_Analysis/${i}/DKI.nii
+  DKIn = $base/03_Analysis/${i}/DKI.nii
+  [ -f "$base/02_Data/${i}/$TOPUP" ] && cp "$base/02_Data/${i}/$TOPUP" "$base/03_Analysis/${i}/DKI_TOPUP.nii"
+  TOPUPn = $base/03_Analysis/${i}/DKI_TOPUP.nii
+  inputs="$DKIn $TOPUPn"
+  pydesigner -o $base/03_Analysis/${i} -s $inputs
 done
